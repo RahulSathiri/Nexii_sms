9175e3ca7214045fdc840e036a27af96f2fc9e40
dariya <dariya@dariya>
2017-07-21 19:32:36 +0530

    Bugs aree fixed - All Updated and Schedule is Returning

--- src/main/java/com/omniwyse/sms/services/TeacherModuleService.java
+++ src/main/java/com/omniwyse/sms/services/TeacherModuleService.java
@@ -1,6 +1,6 @@
 package com.omniwyse.sms.services;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +9,14 @@ import org.springframework.stereotype.Service;
 import com.dieselpoint.norm.Database;
 import com.omniwyse.sms.db.DatabaseRetrieval;
 import com.omniwyse.sms.models.ClassRoom;
+import com.omniwyse.sms.models.SubjectTeacherClass;
 import com.omniwyse.sms.models.Subjects;
 import com.omniwyse.sms.models.Teachers;
-import com.omniwyse.sms.models.TestType;
 import com.omniwyse.sms.utils.ClassRoomDetails;
 import com.omniwyse.sms.utils.ClassSectionTransferObject;
 import com.omniwyse.sms.utils.TeacherModuleDTO;
 import com.omniwyse.sms.utils.TeacherScheduleDTO;
+import com.omniwyse.sms.utils.TestTransferObject;
 
 
 @Service
@@ -33,7 +34,7 @@ public class TeacherModuleService {
 		db = retrive.getDatabase(1);
 
 		List<TeacherModuleDTO> list = db.sql(
-				"select subjects.subjectname, classrooms.gradeid, classrooms.sectionname from subjects "
+				"select classrooms.id, subjects.subjectname, classrooms.gradeid, classrooms.sectionname from subjects "
 						+ "JOIN class_subject_teacher ON class_subject_teacher.subjectid = subjects.id JOIN classrooms"
 						+ " ON classrooms.id = class_subject_teacher.classid where class_subject_teacher.teacherid = ?",
 				moduleDTO.getId()).results(TeacherModuleDTO.class);
@@ -59,19 +60,34 @@ public class TeacherModuleService {
 
 	}
 
-	public List<TeacherScheduleDTO> getSchedule(ClassSectionTransferObject dataObject, Date date) {
+	@SuppressWarnings("deprecation")
+	public List<TeacherScheduleDTO> getSchedule(ClassSectionTransferObject dataObject, String date) {
 
 		db = retrive.getDatabase(1);
 
-		return db
-				.sql("select classroom_periods.periodfrom, classroom_periods.periodto, classrooms.gradeid,"
-						+ " classrooms.sectionname,subjects.subjectname from classrooms JOIN class_subject_teacher"
-						+ " ON class_subject_teacher.classid = classrooms.id JOIN classroom_periods ON "
-						+ "classrooms.id = classroom_periods.classroomid JOIN subjects ON subjects.id = class_subject_teacher.subjectid"
-						+ " where teacherid = ? and dateofassigning = ?", dataObject.getId(), date)
-				.results(TeacherScheduleDTO.class);
-	}
+		List<TeacherScheduleDTO> list = new ArrayList<>();
+		List<SubjectTeacherClass> classub = db
+				.sql("select classid, subjectid from class_subject_teacher where teacherid = ?", dataObject.getId())
+				.results(SubjectTeacherClass.class);
+		for (SubjectTeacherClass sub : classub) {
+			List<TeacherScheduleDTO> sublist = db
+					.sql(" select classroom_periods.periodfrom, classroom_periods.periodto,"
+							+ "subjects.subjectname,classrooms.gradeid,classrooms.sectionname from classroom_periods "
+							+ " join subjects on classroom_periods.subjectid=subjects.id join classrooms on classrooms.id = classroom_periods.classroomid"
+							+ " where classroom_periods.classroomid =? and classroom_periods.subjectid = ? "
+							+ "and classroom_periods.dateofassigning = ?", sub.getClassid(), sub.getSubjectid(), date)
+					.results(TeacherScheduleDTO.class);
+			int variable = 0;
+			for (TeacherScheduleDTO teacher : sublist) {
+				list.add(sublist.get(variable));
+				variable++;
+			}
 
+		}
+
+		return list;
+
+	}
 	public  ClassRoomDetails teacherModuleList(long id, String subjectname) {
 		
 		db = retrive.getDatabase(1);
@@ -81,9 +97,11 @@ public class TeacherModuleService {
 		
 		long subjectid = db.where("subjectname=?",subjectname).results(Subjects.class).get(0).getId();
 		long gradeid=db.where("id=?", id).results(ClassRoom.class).get(0).getGradeid();
-		List<TestType> listTetss=db.sql("select testtype from test_create join test_syllabus on test_create.gradeid=?"
-							+ " and test_syllabus.subjectid=? and test_create.id=test_syllabus.testid join test_type on"
-							+" test_create.testtypeid=test_type.id ", gradeid,subjectid).results(TestType.class);
+		List<TestTransferObject> listTetss = db
+				.sql("select test_type.testtype,test_create.startdate, test_create.enddate from test_create join test_syllabus on test_create.gradeid=?"
+						+ " and test_syllabus.subjectid=? and test_create.id=test_syllabus.testid join test_type on"
+						+ " test_create.testtypeid=test_type.id ", gradeid, subjectid)
+				.results(TestTransferObject.class);
 		
 		classroom.setTests(listTetss);
 		
