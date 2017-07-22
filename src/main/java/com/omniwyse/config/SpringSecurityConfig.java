8f491fb6ca2b7a8e18d8344dd85ab17a5918bc6c
bhaskarm <bhaskarm>
2017-07-21 18:36:27 -1200

    spring security file change

--- src/main/java/com/omniwyse/config/SpringSecurityConfig.java
+++ src/main/java/com/omniwyse/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.omniwyse.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 
 import com.omniwyse.sms.services.MyUserDetailsService;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -24,12 +26,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpsecurity) {
         
         try {
-            httpsecurity.csrf().disable().authorizeRequests().antMatchers("/", "/home", "/about").permitAll()
-                    .antMatchers("/superadmin/**").hasAnyRole("SUPERADMIN").antMatchers("/admin/**")
-                    .hasAnyRole("ADMIN").antMatchers("/teacher/**").hasAnyRole("TEACHER")
-                    .antMatchers("/parent/**").hasAnyRole("PARENT").antMatchers("student/**").hasAnyRole("STUDENT")
-                    .anyRequest().authenticated().and().formLogin().permitAll().and().logout()
-                    .permitAll().and().exceptionHandling().accessDeniedHandler(accesshandler);
+            httpsecurity.csrf().disable().authorizeRequests().antMatchers("**/secured/**").authenticated().anyRequest()
+                    .permitAll().and().formLogin().permitAll();
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
