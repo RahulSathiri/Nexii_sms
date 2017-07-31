30bc9b98c6e5b711bb738f1cb0c07d970925a7ce
lakshmisreeneela <lakshmigudugunuri6@gmail.com>
2017-07-31 14:10:51 +0530

    adding tenantid for home

--- src/main/java/com/omniwyse/sms/controller/ActivityController.java
+++ src/main/java/com/omniwyse/sms/controller/ActivityController.java
@@ -3,6 +3,7 @@ package com.omniwyse.sms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,9 @@ public class ActivityController {
     @Autowired
     private ActivitiesService service;
 
-    @RequestMapping("/dashboard")
-    public ResponseEntity<DashBoard> activities() {
-        DashBoard dashboard = service.listOfActivities();
+    @RequestMapping("/{tenantId}/dashboard")
+    public ResponseEntity<DashBoard> activities(@PathVariable("tenantId") long tenantId) {
+        DashBoard dashboard = service.listOfActivities(tenantId);
         return new ResponseEntity<DashBoard>(dashboard, HttpStatus.OK);
     }
 
