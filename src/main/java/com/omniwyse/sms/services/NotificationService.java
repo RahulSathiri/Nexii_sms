44854d5bdaaae7a34f3738eeb5d7c0feaaf265d6
dariyahussain220 <dariyahussain220@gmail.com>
2017-08-19 19:22:27 +0530

    commit after adding publish notification on Actioncode based

--- src/main/java/com/omniwyse/sms/services/NotificationService.java
+++ src/main/java/com/omniwyse/sms/services/NotificationService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.dieselpoint.norm.Database;
 import com.dieselpoint.norm.Transaction;
 import com.omniwyse.sms.db.DatabaseRetrieval;
+import com.omniwyse.sms.models.Lessons;
 import com.omniwyse.sms.models.Notifications;
 import com.omniwyse.sms.utils.NotificationsDTO;
 
@@ -29,6 +30,7 @@ public class NotificationService {
 		try {
 			notifications.setNotificationname(data.getNotificationname());
 			notifications.setDescription(data.getDescription());
+			checkActioncode(data);
 			notifications.setActioncode(data.getActioncode());
 			notifications.setParentactionrequired(data.getParentactionrequired());
 			notifications.setPublishedby(data.getPublishedby());
@@ -41,6 +43,21 @@ public class NotificationService {
 			return -1;
 		}
 		return rowEffected;
+	}
+
+	private void checkActioncode(NotificationsDTO data) {
+
+		long id = data.getId();
+		if (data.getActioncode().equals("Timeline")) {
+
+			db.sql("upadte lessons set publishtimeline = true").where("id = ?", id).results(Lessons.class);
+
+		} /*
+			 * else if (data.getActioncode().equals("Tests")) {
+			 * 
+			 * db.update("").where("id = ?", id); }
+			 */
+
 	}
 
 	public List<NotificationsDTO> listAllPublishednNotifications(long tenantid, NotificationsDTO data) {
