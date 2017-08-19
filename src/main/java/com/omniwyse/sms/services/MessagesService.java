6796dfa43b10f2a25cbc5d816ddf8d1c712bdd81
lakshmisreeneela <lakshmigudugunuri6@gmail.com>
2017-08-19 13:12:44 +0530

    commit

--- src/main/java/com/omniwyse/sms/services/MessagesService.java
+++ src/main/java/com/omniwyse/sms/services/MessagesService.java
@@ -65,10 +65,10 @@ public class MessagesService {
 		db = retrive.getDatabase(tenantId);
 		List<MessagesDetails> messages = db.sql("select messages.id,messages.message,messages.classroomid,messages.messagedate,messages.senderid,messages.recieverid,students.name,parents.fathername,parents.mothername "
 						+ "from messages left join students on students.id=messages.recieverid left join parents on parents.id=students.parentid"
-						+ " where messages.sentflag='T' and messages.senderid=? and messages.rootmessageid=0 order by messages.messagedate desc ",messagesDTO.getSenderid()).results(MessagesDetails.class);
+						+ " where messages.sentflag='T' and messages.senderid=? and messages.classroomid=? and messages.rootmessageid=0 order by messages.messagedate desc ",messagesDTO.getSenderid(),messagesDTO.getClassroomid()).results(MessagesDetails.class);
 
 		for (MessagesDetails message : messages) {
-			List<MessagesDetails> replymessages = db.sql("select messages.message,messages.senderid,messages.classroomid,messages.recieverid,messages.messagedate from messages"
+			List<MessagesDetails> replymessages = db.sql("select messages.id,messages.message,messages.senderid,messages.classroomid,messages.recieverid,messages.messagedate from messages"
 			        + " where parentmessageid=? order by messagedate asc", message.getId()).results(MessagesDetails.class);
 			message.setReplymessages(replymessages);
 		}
@@ -81,11 +81,11 @@ public class MessagesService {
 		db = retrive.getDatabase(tenantId);
 
 		List<MessagesDetails> messages = db.sql("select messages.id,messages.classroomid,messages.message,messages.messagedate,messages.recieverid,messages.senderid,teachers.teachername "
-						+ "from messages join teachers on teachers.id=messages.recieverid where messages.sentflag='P' and messages.senderid=? and messages.rootmessageid=0"
-						+ " order by messages.messagedate desc", messagesDTO.getSenderid()).results(MessagesDetails.class);
+						+ "from messages join teachers on teachers.id=messages.recieverid where messages.sentflag='P' and messages.senderid=? and and messages.classroomid=? and messages.rootmessageid=0"
+						+ " order by messages.messagedate desc", messagesDTO.getSenderid(),messagesDTO.getClassroomid()).results(MessagesDetails.class);
 
 		for (MessagesDetails message : messages) {
-			List<MessagesDetails> replymessages = db.sql("select messages.message,messages.classroomid,messages.messagedate from messages where messages.parentmessageid=? order by messagedate asc",
+			List<MessagesDetails> replymessages = db.sql("select messages.id,messages.message,messages.classroomid,messages.messagedate from messages where messages.parentmessageid=? order by messagedate asc",
 							message.getId()).results(MessagesDetails.class);
 
 			message.setReplymessages(replymessages);
@@ -96,8 +96,8 @@ public class MessagesService {
 	public List<MessagesDetails> teacherRecievedMessages(MessagesDTO messagesDTO, long tenantId) {
 		db = retrive.getDatabase(tenantId);
 		List<MessagesDetails> messages= db.sql("select messages.id,messages.message,messages.messagedate,messages.rootmessageid,messages.senderid,messages.recieverid,students.name,parents.fathername,parents.mothername "
-                        + "from messages join students on students.id=messages.senderid join parents on parents.id=students.parentid where sentflag='p' and recieverid=? and rootmessageid=0 order by messages.messagedate desc",
-						   messagesDTO.getRecieverid()).results(MessagesDetails.class);
+                        + "from messages join students on students.id=messages.senderid join parents on parents.id=students.parentid where sentflag='p' and recieverid=? and classroomid=? and rootmessageid=0 order by messages.messagedate desc",
+						   messagesDTO.getRecieverid(),messagesDTO.getClassroomid()).results(MessagesDetails.class);
 		for (MessagesDetails message : messages) {
 			List<MessagesDetails> replymessages = db.sql("select messages.message,messages.senderid,messages.recieverid,messages.classroomid,messages.messagedate from messages"
 			        + " where messages.parentmessageid=? order by messagedate asc",message.getId()).results(MessagesDetails.class);
