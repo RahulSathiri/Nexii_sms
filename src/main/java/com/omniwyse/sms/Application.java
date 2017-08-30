54522ce22899751e0ff9318d3a0174591d84d70f
dariyahussain220 <dariyahussain220@gmail.com>
2017-08-30 15:45:46 +0530

    commit after adding updated code in application start

--- src/main/java/com/omniwyse/sms/Application.java
+++ src/main/java/com/omniwyse/sms/Application.java
@@ -7,14 +7,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.omniwyse.sms.config.ListObjects;
+
 @SpringBootApplication
 public class Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String ar[]) throws IOException {
-        LOGGER.info("Starting sms API");
+        LOGGER.info("*** Starting sms API ****");
         SpringApplication.run(Application.class, ar);
+        ListObjects listObj = new ListObjects();
+        LOGGER.info("**** Updating iSchool Worksheets Library *****");
+		listObj.listObjects("ischool-sms");
     }
 
 }
--- src/main/java/com/omniwyse/sms/config/ListObjects.java
+++ src/main/java/com/omniwyse/sms/config/ListObjects.java
@@ -1,7 +1,10 @@
 package com.omniwyse.sms.config;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,34 +12,58 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.dieselpoint.norm.Database;
+import com.dieselpoint.norm.Transaction;
+import com.omniwyse.sms.db.DBFactory;
+import com.omniwyse.sms.ischool.IschoolGrades;
+import com.omniwyse.sms.ischool.IschoolLevelsOfDifficulty;
+import com.omniwyse.sms.ischool.IschoolSubjects;
+import com.omniwyse.sms.ischool.IschoolWorksheets;
 
 @EnableAutoConfiguration
 @Configuration
 public class ListObjects {
 
+	private Database db;
+	
+	@Autowired
+	private DBFactory database;
+	
 	public void listObjects(String bucket_name) {
 
+		db = database.getSchoolDb();
 		System.out.format("Objects in S3 bucket %s:\n", bucket_name);
 		final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
 		ObjectListing ol = s3.listObjects(bucket_name);
 		List<S3ObjectSummary> objects = ol.getObjectSummaries();
-		for (S3ObjectSummary os : objects) {
-			String[] str = os.getKey().split("/");
-			if (str.length == 4) {
-				System.out.println("* " + os.getKey());
-				for (int i = 0; i < str.length; i++) {
-					if (str[i] == "1") {
+		List<IschoolGrades> grades = db.sql("select * from ischool_grades").results(IschoolGrades.class);
+		List<IschoolSubjects> subjects = db.sql("select * from ischool_subjects").results(IschoolSubjects.class);
+		List<IschoolLevelsOfDifficulty> level = db.sql("select * from ischool_degreeofdifficulty").results(IschoolLevelsOfDifficulty.class);
+		
+		Transaction transaction = db.startTransaction();
+		try {
+			IschoolWorksheets worksheetsLibrary = new IschoolWorksheets();
+			Map<Integer, String[]> hm = new HashMap<Integer, String[]>();
+			int temp = 0;
+			for (S3ObjectSummary os : objects) {
+				String[] str = os.getKey().split("/");
+				if (str.length == 4) {
+					temp++;
+					System.out.println("* " + os.getKey());
+					hm.put(temp, str);
 
-					}
+				} else {
+					continue;
 				}
-			} else {
-				continue;
 			}
+			for (int i = 0; i < hm.size(); i++) {
+
+			}
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+			System.out.println("Exception Occured");
 		}
-	}
-	
-	public static void main(String[] args) {
-		ListObjects listObj = new ListObjects();
-        listObj.listObjects("ischool-sms");
+
 	}
 }
